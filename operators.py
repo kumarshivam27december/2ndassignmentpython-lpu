0204f0239cc61bf64a660ee38cb41f0bce21f0cb
shivamkumar <kumarshivam16062004@gmail.com>
2022-10-16 13:42:12 +0530

    version 1.0

---
+++ operators.py
@@ -0,0 +1,74 @@
+# +	Add two operands or unary plus	x + y+ 2
+# -	Subtract right operand from the left or unary minus	x - y- 2
+# *	Multiply two operands	x * y
+# /	Divide left operand by the right one (always results into float)
+# x / y
+# %	Modulus - remainder of the division of left operand by the right	
+# x % y (remainder of x/y)
+# //	Floor division - division that results into whole number 
+# adjusted to the left in the number line	x // y
+# **	Exponent - left operand raised to the power of right
+# x**y (x to the power y)
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print('x / y =',x/y)
+
+# Output: x // y = 3
+print('x // y =',x//y)
+
+# Output: x ** y = 50625
+print('x ** y =',x**y)
+
+# >	Greater than - True if left operand is greater than the right	
+# x > y
+# <	Less than - True if left operand is less than the right	x < y
+# ==	Equal to - True if both operands are equal	x == y
+# !=	Not equal to - True if operands are not equal	x != y
+# >=	Greater than or equal to - True if left operand is greater
+# than or equal to the right	x >= y
+# <=	Less than or equal to - True if left operand is less than
+# or equal to the right	x <= y
+
+m = 10
+n = 12
+
+# Output: m > n is False
+print('m > n is',m>n)
+
+# Output: m < n is True
+print('m < n is',m<n)
+
+# Output: m == n is False
+print('m == n is',m==n)
+
+# Output: m != n is True
+print('m != n is',m!=n)
+
+# Output: m >= n is False
+print('m >= n is',m>=n)
+
+# Output: m <= n is True
+print('m <= n is',m<=n)
+
+
+
+
+
+
+
+
+
+
+
+
